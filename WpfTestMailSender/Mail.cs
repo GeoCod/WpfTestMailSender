d460e289aa93ff3f246dc5e662afebc5e66320d3
GeoCod <artem.g.vasilchenko@gmail.com>
2020-11-29 12:29:57 +0300

    Перенес логику отправки почты в отдельниый класс

---
+++ WpfTestMailSender/Mail.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Mail;
+using System.Windows;
+
+namespace WpfTestMailSender
+{
+    public static class Mail
+    {
+        public static List<string> GetMail(string text)
+        {
+            return new List<string>();
+        }
+
+        public static void SendMail(string login, string password, string address)
+        {
+            try
+            {
+                // Используем using, чтобы гарантированно удалить объекты mail и client после использования
+                using (MailMessage mail = new MailMessage(login, address))
+                {
+                    // Формируем письмо
+                    mail.From = new MailAddress(login);     // Адрес отправителя
+                    mail.To.Add(new MailAddress(address));  // Адрес получателя
+                    mail.Subject = "Привет из C#";
+                    mail.Body = "Hello, world!";
+
+                    SmtpClient client = new SmtpClient();
+                    client.Host = "smtp.yandex.ru";
+                    client.Port = 587;
+                    client.Timeout = 5000;
+                    client.EnableSsl = true;
+                    client.Credentials = new NetworkCredential(login, password);
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Невозможно отправить письмо " + ex.ToString());
+            }
+
+            SendEndWindow sew = new SendEndWindow();
+            sew.ShowDialog();
+        }
+    }
+}
--- WpfTestMailSender/WpfMailSender.xaml.cs
+++ WpfTestMailSender/WpfMailSender.xaml.cs
@@ -15,37 +15,11 @@ namespace WpfTestMailSender
 
         private void btnSendEmail_Click(object sender, RoutedEventArgs e)
         {
-            List<string> listStrMails = new List<string> { "test.textovitch@yandex.ru" };  // Список email'ов кому мы отправляем письмо
-            string strPassword = passwordBox.Password;
-            foreach (string mail in listStrMails)
-            {
-                // Используем using, чтобы гарантированно удалить объект MailMessage после использования
-                using (MailMessage mm = new MailMessage("test.textovitch@yandex.ru", mail))
-                {
-                    // Формируем письмо
-                    mm.Subject = "Привет из C#"; // Заголовок письма
-                    mm.Body = "Hello, world!";       // Тело письма
-                    mm.IsBodyHtml = false;           // Не используем html в теле письма
-                                                     // Авторизуемся на smtp-сервере и отправляем письмо
-                                                     // Оператор using гарантирует вызов метода Dispose, даже если при вызове 
-                                                     // методов в объекте происходит исключение.
-                    using (SmtpClient sc = new SmtpClient("smtp.yandex.ru", 587))
-                    {
-                        sc.EnableSsl = true;
-                        sc.Credentials = new NetworkCredential("test.textovitch@yandex.ru", strPassword);
-                        try
-                        {
-                            sc.Send(mm);
-                        }
-                        catch (Exception ex)
-                        {
-                            MessageBox.Show("Невозможно отправить письмо " + ex.ToString());
-                        }
-                    }
-                }
-            }
-            SendEndWindow sew = new SendEndWindow();
-            sew.ShowDialog();
+            var login = LoginTb.Text;
+            var password = passwordBox.Password;
+            var address = AddressTb.Text;
+
+            Mail.SendMail(login, password, address);
         }
     }
 }
