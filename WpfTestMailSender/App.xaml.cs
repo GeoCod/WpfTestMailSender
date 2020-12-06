88f73f73cfdc51a7e8cfdd2317588540e0a7bde4
GeoCod <artem.g.vasilchenko@gmail.com>
2020-12-06 13:03:37 +0300

    Добавлен ViewModelLocator

--- WpfTestMailSender/App.xaml.cs
+++ WpfTestMailSender/App.xaml.cs
@@ -1,17 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
 using System.Windows;
+using WpfTestMailSender.ViewModels;
 
 namespace WpfTestMailSender
 {
-    /// <summary>
-    /// Логика взаимодействия для App.xaml
-    /// </summary>
+    /// <summary>Логика взаимодействия для App.xaml</summary>
     public partial class App : Application
     {
+        private static IHost _hosting;
+
+        public static IHost Hosting
+        {
+            get
+            {
+                if (_hosting != null) return _hosting;
+                _hosting = Host.CreateDefaultBuilder(Environment.GetCommandLineArgs()).ConfigureServices(ConfigureServices).Build();
+                return _hosting;
+            }
+        }
+
+        private static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
+        {
+//#if DEBUG
+//            services.AddTransient<IMailService, DebugMailService>();
+//#else
+//            services.AddTransient<IMailService, SmtpMailService>();
+//#endif
+
+            services.AddSingleton<MainViewModel>();
+
+        }
+
+        public static IServiceProvider Services { get { return Hosting.Services; } }
     }
 }
---
+++ WpfTestMailSender/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace WpfTestMailSender.ViewModels
+{
+    class ViewModelLocator
+    {
+        public MainViewModel MainWindowModel => App.Services.GetRequiredService<MainViewModel>();
+    }
+}
